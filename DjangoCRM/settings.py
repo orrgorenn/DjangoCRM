3e247fc3d8fbb2261f8d37b00429e61a8a33b83d
Orr Goren <orrgorenn@gmail.com>
2020-11-09 13:56:27 +0200

    Added env

--- DjangoCRM/settings.py
+++ DjangoCRM/settings.py
@@ -11,20 +11,27 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from secrets import *
+# from secrets import SECRET_KEY
 import os
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG = (bool, False)
+)
+
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = DEBUG = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
