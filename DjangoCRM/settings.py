3d3e00a1ddd56457ffa937e3d103c11b77e2e6ed
orrgorenn <orrgorenn@gmail.com>
2020-11-11 14:14:52 +0200

    Style changing

--- DjangoCRM/settings.py
+++ DjangoCRM/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-SECRET_KEY = DEBUG = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
